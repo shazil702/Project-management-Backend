8c06d3edbd1e985f384c003a97cc33b0f8a8a56d
Shazil <shazilva2@gmail.com>
2024-08-01 00:39:09 +0530

    Added Admin View

---
+++ Authentication/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('admintoken/', views.AdminTokenView.as_view()),
+]
--- Authentication/views.py
+++ Authentication/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import AdminToken
+from .models import User
+from rest_framework.response import Response
 
-# Create your views here.
+class AdminTokenView(TokenObtainPairView):
+    serializer_class = AdminToken
+    def post(self, request, *args, **kwargs):
+        user = User.objects.get(email = request.data['email'])
+        print(user.email)
+        print(user.is_superuser)
+        if not user.is_superuser:
+            return Response({"message":"user is not super user"}, status=status.HTTP_403_FORBIDDEN)
+        response = super().post(request, *args, **kwargs)
+        return response
+    def get_serializer_context(self):
+        return super().get_serializer_context()
