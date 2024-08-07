7a2f9687fb5deed2d00888e22ef85bbe8e5ade71
Shazil <shazilva2@gmail.com>
2024-08-07 23:16:12 +0530

    created Project Model

--- Authentication/serializers.py
+++ Authentication/serializers.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id','username', 'email', 'is_superuser', 'is_hr', 'is_tl', 'is_employee', 'phone']
 
 class AdminToken(TokenObtainPairSerializer):
     @classmethod
